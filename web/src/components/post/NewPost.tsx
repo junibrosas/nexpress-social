bb519814f8cecae6b5d4672f013b2914c5d22e19
Juni Brosas <j.brosas@arcanys.com>
2020-06-14 01:02:03 +0800

    fixed unable to delete a post

--- web/src/components/post/NewPost.tsx
+++ web/src/components/post/NewPost.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core';
-import { Card, CardHeader, CardContent, CardActions, IconButton, Button, Typography, Avatar, Icon, TextField } from '@material-ui/core';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  IconButton,
+  Button,
+  Typography,
+  Avatar,
+  Icon,
+  TextField,
+} from '@material-ui/core';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
 import { PostApiService } from 'src/services/postapi.service';
 import { AuthHelper } from 'src/helpers/auth.helper';
@@ -18,46 +29,46 @@ interface IState {
   user: any;
 }
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
     backgroundColor: '#efefef',
-    padding: `${theme.spacing.unit*3}px 0px 1px`
+    padding: `${theme.spacing.unit * 3}px 0px 1px`,
   },
   card: {
-    maxWidth:600,
+    maxWidth: 600,
     margin: 'auto',
-    marginBottom: theme.spacing.unit*3,
+    marginBottom: theme.spacing.unit * 3,
     backgroundColor: 'rgba(65, 150, 136, 0.09)',
-    boxShadow: 'none'
+    boxShadow: 'none',
   },
   cardContent: {
     backgroundColor: 'white',
     paddingTop: 0,
-    paddingBottom: 0
+    paddingBottom: 0,
   },
   cardHeader: {
     paddingTop: 8,
-    paddingBottom: 8
+    paddingBottom: 8,
   },
   photoButton: {
     height: 30,
-    marginBottom: 5
+    marginBottom: 5,
   },
   input: {
     display: 'none',
   },
   textField: {
-    marginLeft: theme.spacing.unit*2,
-    marginRight: theme.spacing.unit*2,
-    width: '90%'
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    width: '90%',
   },
   submit: {
-    margin: theme.spacing.unit * 2
+    margin: theme.spacing.unit * 2,
   },
-  filename:{
-    verticalAlign: 'super'
-  }
-})
+  filename: {
+    verticalAlign: 'super',
+  },
+});
 
 class NewPostComponent extends React.Component<IProps, IState> {
   postData: FormData;
@@ -69,52 +80,75 @@ class NewPostComponent extends React.Component<IProps, IState> {
       text: '',
       photo: '',
       error: '',
-      user: {}
-    }
+      user: {},
+    };
   }
 
   render() {
-    const {classes} = this.props;
+    const { classes } = this.props;
 
     return (
       <div className={classes.root}>
         <Card className={classes.card}>
-        <CardHeader
-              avatar={
-                <Avatar src={UserApiService.getPhotoUrl(this.state.user._id)}/>
-              }
-              title={this.state.user.name}
-              className={classes.cardHeader}
-            />
-        <CardContent className={classes.cardContent}>
-          <TextField
-              placeholder="Share your thoughts ..."
+          <CardHeader
+            avatar={
+              <Avatar src={UserApiService.getPhotoUrl(this.state.user._id)} />
+            }
+            title={this.state.user.name}
+            className={classes.cardHeader}
+          />
+          <CardContent className={classes.cardContent}>
+            <TextField
+              placeholder='Share your thoughts ...'
               multiline
-              rows="3"
+              rows='3'
               value={this.state.text}
               onChange={this.handleChange('text')}
               className={classes.textField}
-              margin="normal"
-          />
-          <input accept="image/*" onChange={this.handleChange('photo')} className={classes.input} id="icon-button-file" type="file" />
-          <label htmlFor="icon-button-file">
-            <IconButton color="secondary" className={classes.photoButton} component="span">
-              <PhotoCamera />
-            </IconButton>
-          </label> <span className={classes.filename}>{this.state.photo ? this.state.photo.name : ''}</span>
-          { this.state.error && (
-            <Typography component="p" color="error">
-              <Icon color="error" className={classes.error}>error</Icon>
+              margin='normal'
+            />
+            <input
+              accept='image/*'
+              onChange={this.handleChange('photo')}
+              className={classes.input}
+              id='icon-button-file'
+              type='file'
+            />
+            <label htmlFor='icon-button-file'>
+              <IconButton
+                color='secondary'
+                className={classes.photoButton}
+                component='span'
+              >
+                <PhotoCamera />
+              </IconButton>
+            </label>{' '}
+            <span className={classes.filename}>
+              {this.state.photo ? this.state.photo.name : ''}
+            </span>
+            {this.state.error && (
+              <Typography component='p' color='error'>
+                <Icon color='error' className={classes.error}>
+                  error
+                </Icon>
                 {this.state.error}
-            </Typography>
-          )}
-        </CardContent>
-        <CardActions>
-          <Button color="primary" variant="contained" disabled={this.state.text === ''} onClick={this.clickPost} className={classes.submit}>POST</Button>
-        </CardActions>
-      </Card>
-    </div>
-    )
+              </Typography>
+            )}
+          </CardContent>
+          <CardActions>
+            <Button
+              color='primary'
+              variant='contained'
+              disabled={this.state.text === ''}
+              onClick={this.clickPost}
+              className={classes.submit}
+            >
+              POST
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
+    );
   }
 
   componentDidMount() {
@@ -122,27 +156,28 @@ class NewPostComponent extends React.Component<IProps, IState> {
     this.setState({ user: AuthHelper.isAuthenticated().user });
   }
 
-  handleChange = name => event => {
+  handleChange = (name) => (event) => {
     const value = name === 'photo' ? event.target.files[0] : event.target.value;
     this.postData.set(name, value);
-    this.setState(({ [name]: value } as any));
-  }
+    this.setState({ [name]: value } as any);
+  };
 
-  clickPost = () => {
+  clickPost = async () => {
     const jwt = AuthHelper.isAuthenticated();
 
-    PostApiService.create(
+    const data = await PostApiService.create(
       { userId: jwt.user._id },
-      { t: jwt.token},
-      this.postData).then((data) => {
-        if (data && data.error) {
-          this.setState({ error: data.error });
-        } else {
-          this.setState({ text: '', photo: '' });
-          this.props.addUpdate(data);
-        }
-      });
-  }
+      { t: jwt.token },
+      this.postData
+    );
+
+    if (data && data.error) {
+      this.setState({ error: data.error });
+    } else {
+      this.setState({ text: '', photo: '' });
+      this.props.addUpdate(data);
+    }
+  };
 }
 
-export const NewPost = withStyles(styles)(NewPostComponent)
+export const NewPost = withStyles(styles)(NewPostComponent);
--- web/src/services/postapi.service.ts
+++ web/src/services/postapi.service.ts
@@ -5,128 +5,152 @@ const endpoint = publicRuntimeConfig.api.endpoint;
 
 class ApiService {
   create = (params, credentials, post) => {
-    return fetch(`${endpoint}/posts/new/`+ params.userId, {
+    return fetch(`${endpoint}/posts/new/` + params.userId, {
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
-        'Authorization': 'Bearer ' + credentials.t
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + credentials.t,
       },
-      body: post
-    }).then((response) => {
-      return response.json()
-    }).catch((err) => {
-      console.log(err)
-    });
-  }
+      body: post,
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   listNewsFeed = (params, credentials) => {
     return fetch(`${endpoint}/posts/feed/${params.userId}`, {
       method: 'GET',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + credentials.t
-      }
-    }).then(response => {
-      return response.json();
-    }).catch((err) => console.log(err))
-  }
+        Authorization: 'Bearer ' + credentials.t,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => console.log(err));
+  };
 
   listByUser = (params, credentials) => {
     return fetch(`${endpoint}/posts/by/${params.userId}`, {
       method: 'GET',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + credentials.t
-      }
-    }).then(response => {
-      return response.json()
-    }).catch((err) => console.log(err))
-  }
+        Authorization: 'Bearer ' + credentials.t,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => console.log(err));
+  };
 
   remove = (params, credentials) => {
-    return fetch(`${endpoint}/posts/${params.userId}`, {
+    return fetch(`${endpoint}/posts/${params.postId}`, {
       method: 'DELETE',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + credentials.t
-      }
-    }).then((response) => {
-      return response.json()
-    }).catch((err) => {
-      console.log(err)
+        Authorization: 'Bearer ' + credentials.t,
+      },
     })
-  }
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   like = (params, credentials, postId) => {
     return fetch(`${endpoint}/posts/like/`, {
       method: 'PUT',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + credentials.t
+        Authorization: 'Bearer ' + credentials.t,
       },
-      body: JSON.stringify({userId:params.userId, postId: postId})
-    }).then((response) => {
-      return response.json()
-    }).catch((err) => {
-      console.log(err)
+      body: JSON.stringify({ userId: params.userId, postId: postId }),
     })
-  }
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   unlike = (params, credentials, postId) => {
     return fetch(`${endpoint}/posts/unlike/`, {
       method: 'PUT',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + credentials.t
+        Authorization: 'Bearer ' + credentials.t,
       },
-      body: JSON.stringify({userId:params.userId, postId: postId})
-    }).then((response) => {
-      return response.json()
-    }).catch((err) => {
-      console.log(err)
+      body: JSON.stringify({ userId: params.userId, postId: postId }),
     })
-  }
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   comment = (params, credentials, postId, comment) => {
     return fetch(`${endpoint}/posts/comment/`, {
       method: 'PUT',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + credentials.t
+        Authorization: 'Bearer ' + credentials.t,
       },
-      body: JSON.stringify({userId:params.userId, postId: postId, comment: comment})
-    }).then((response) => {
-      return response.json()
-    }).catch((err) => {
-      console.log(err)
+      body: JSON.stringify({
+        userId: params.userId,
+        postId: postId,
+        comment: comment,
+      }),
     })
-  }
-  
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   uncomment = (params, credentials, postId, comment) => {
     return fetch(`${endpoint}/posts/uncomment/`, {
       method: 'PUT',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + credentials.t
+        Authorization: 'Bearer ' + credentials.t,
       },
-      body: JSON.stringify({userId:params.userId, postId: postId, comment: comment})
-    }).then((response) => {
-      return response.json()
-    }).catch((err) => {
-      console.log(err)
+      body: JSON.stringify({
+        userId: params.userId,
+        postId: postId,
+        comment: comment,
+      }),
     })
-  }
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   getPhotoUrl = (photoId) => {
     return `${endpoint}/posts/photo/${photoId}`;
-  }
-} 
+  };
+}
 
 export const PostApiService = new ApiService();
